5dec661214e5ba3bbbe30ff2d482355af6a875cb
hohno-46466 <hohno.46466@gmail.com>
2022-11-17 17:21:06 +0900

    updated by @hohno_at_kuimc on hohno-MBP-2022P.local

--- examples/our3rdSketch/our3rdSketch.ino
+++ examples/our3rdSketch/our3rdSketch.ino
@@ -2,12 +2,22 @@
 // our3rdSketch.ino
 //
 
+// Last update: Thu Nov 17 17:17:17 JST 2022
+
+const int buttonPin = 6;
+
 void setup() {
   Serial.begin(57600);
+  pinMode(buttonPin, INPUT);
+  randomSeed(micros());
 }
 
 void loop() {
-  Serial.print("Hello, World!\t");
-  Serial.println(millis());
-  delay(1000);
+  static int num = 100;
+  Serial.print("Hello, World!"); Serial.print("\t");
+  Serial.print(millis());      Serial.print("\t");
+  Serial.print((digitalRead(buttonPin) == HIGH) ? "ON" : "OFF"); Serial.print("\t");
+  Serial.print(num); Serial.println();
+  num += random(-50,50);
+  delay(200);
 }
--- examples/ourNextSketch/ourNextSketch.ino
+++ examples/ourNextSketch/ourNextSketch.ino
@@ -2,18 +2,18 @@
 // ourNextSketch.ino
 //
 
+// Last update: Thu Nov 17 17:14:59 JST 2022
+
 const int buttonPin = 6;
 
 void setup() {
   Serial.begin(57600);
   pinMode(buttonPin, INPUT);
-
 }
 
 void loop() {
-  Serial.print("Hello, world!\t");
-  Serial.print(millis());
-  Serial.print((digitalRead(buttonPin) == HIGH) ? "\tON" : "\tOFF");
-  Serial.println();
-  delay(100);
+  Serial.print("Hello, World!"); Serial.print("\t");
+  Serial.print(millis());      Serial.print("\t");
+  Serial.print((digitalRead(buttonPin) == HIGH) ? "ON" : "OFF"); Serial.println();
+  delay(200);
 }
