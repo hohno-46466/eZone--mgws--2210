8bf21368687b452f435ce5153ef606c14a2039bb
hohno-46466 <hohno.46466@gmail.com>
2023-11-17 05:47:44 +0900

    updated by @hohno_at_kuimc on hohno-AX3-2020J

--- examples/ourSerialRead1/ourSerialRead1.ino
+++ examples/ourSerialRead1/ourSerialRead1.ino
@@ -4,20 +4,26 @@
 
 // Last update: Thu Dec 15 21:06:00 JST 2022
 
-int val1 = 0, val2 = 0, val3 = 0;
+// int val1 = 0, val2 = 0, val3 = 0;
+float val1 = 0.0, val2 = 0.0, val3 = 0.0;
 
 void setup() {
   Serial.begin(57600);
   Serial.print("#debug:\tHello, Kanazawa!\n");
 }
 
+// float tmpV1 = 0.0, tmpV2 = 0.0, tmpV3 = 0.0;
+int n, tmpV1 = 0, tmpV2 = 0, tmpV3 = 0;
+
 void loop() {
   if (Serial.available() > 0) {
     String str = Serial.readStringUntil('\n');
-    int n, tmpV1 = 0, tmpV2 = 0, tmpV3 = 0;
+
+    int n;
+
     Serial.print("#debug str\t["); Serial.print(str); Serial.println("]");
 
-    if ((n = sscanf(str.c_str(), "%d %d %d", &tmpV1, &tmpV2, &tmpV3)) >= 1) {
+    if ((n = sscanf(str.c_str(), "%d %d %d", &tmpV1, &tmpV2, &tmpV3)) >= 0) {
       val1 = tmpV1;
       if (n >= 2) {
         val2 = tmpV2;
@@ -27,10 +33,9 @@ void loop() {
       } 
       Serial.print("#debug:\t");
       Serial.print(millis()); Serial.print("\t");
-      Serial.print(val1); Serial.print("\t");
-      Serial.print(val2); Serial.print("\t");
-      Serial.print(val3); Serial.println();
+      Serial.print(tmpV1/*val1*/); Serial.print("\t");
+      Serial.print(tmpV2/*val2*/); Serial.print("\t");
+      Serial.print(tmpV3/*val3*/); Serial.println();
     }
   }
 }
-
